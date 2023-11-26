f3fc855a7cf2de0992525a5f1a9cba59f9320a6e
pedrojschmidt <pedro.schmidt@ing.austral.edu.ar>
2023-11-26 13:27:20 -0300

    test with front v1 working correctly

--- index.js
+++ index.js
@@ -8,12 +8,20 @@ app.use(cors(
     {
         origin: '*',
     }
-));
+))
+
+// Middleware para parsear el cuerpo de las solicitudes como JSON
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('¡Hola, mundo!');
-});
+})
+
+app.post('/buy', (req, res) => {
+    console.log('Buy ' + req.body.topic);
+    res.send(req.body.topic + ' bought');
+})
 
 app.listen(port, () => {
-    console.log('Server listening on port' + port);
+    console.log('Server listening on port ' + port);
 })
